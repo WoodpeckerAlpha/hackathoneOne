3c69df8df61bf224dd97279efe24b96a35320890
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2024-09-22 13:21:39 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,52 @@
 import './styles.css'
 
 import { ContextMenu } from './menu'
+import {BackgroundModule} from './modules/background.module'
+import {ClicksModule} from './modules/clicks.module'
+import { ShapeModule } from './modules/shape.module'
+import { RandomFigure } from './modules/random.figure.module'
 
 const menu = new ContextMenu('.menu')
-menu.open()
+
+const modules = {
+    randomFigure: new RandomFigure('randomFigure', 'randomFigure'),
+    shapeModule: new ShapeModule('shapeModule', 'shape'),
+    clicksModule: new ClicksModule('clicksModule', 'clicks'),
+    backgroundModule: new BackgroundModule('backgroundModule', 'background')
+}
+
+document.body.addEventListener('contextmenu', (event) => {
+    event.preventDefault()
+    const items = document.querySelectorAll('.menu-item')
+    if (items.length !== 0) {
+        items.forEach((el) => {
+            el.remove()
+        })
+    } 
+
+
+    Object.assign(menu.el.style, {
+        top: `${event.clientY}px`,
+        left: `${event.clientX}px`
+    })
+
+    const objKeys = Object.keys(modules)
+    if (objKeys.length !== 0) {
+        menu.open()
+        menu.add(modules['randomFigure'])
+        menu.add(modules['shapeModule'])
+        menu.add(modules['clicksModule'])
+        menu.add(modules['backgroundModule'])
+    }
+
+})
+
+menu.el.addEventListener('click', (event) => {
+    const module = document.querySelector('.module')
+    if (module) {
+        module.remove()
+    }
+    const {type} = event.target.dataset
+    modules[type].trigger()
+    menu.close()
+})
