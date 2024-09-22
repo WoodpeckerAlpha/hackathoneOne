edd859d6f9a3dd64691808f72ecc1455cf4f800f
WoodpeckerAlpha <woodpeckeralhpa@mail.ru>
2024-09-22 21:33:20 +0400

    add background color change, clicker module, guess number game

--- src/app.js
+++ src/app.js
@@ -1,60 +1,61 @@
-import './styles.css'
+import "./styles.css";
 
-import { ContextMenu } from './menu'
-import { BackgroundModule } from './modules/background.module'
-import { ClicksModule } from './modules/clicks.module'
-import { ShapeModule } from './modules/shape.module'
-import { RandomFigure } from './modules/random.figure.module'
-import { RandomAudio } from './modules/random.audio.module'
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { ShapeModule } from "./modules/shape.module";
+import { RandomFigure } from "./modules/random.figure.module";
+import { RandomAudio } from "./modules/random.audio.module";
+import { SetBackgroundWhiteModule } from "./modules/setBackgroundWhite.module";
+import { GuessNumberModule } from "./modules/guessNumber.module";
 
-const menu = new ContextMenu('.menu')
+const menu = new ContextMenu(".menu");
 
 const modules = {
-    randomAudio: new RandomAudio('randomAudio', 'Рандомный звук'),
-    randomFigure: new RandomFigure('randomFigure', 'randomFigure'),
-    shapeModule: new ShapeModule('shapeModule', 'shape'),
-    clicksModule: new ClicksModule('clicksModule', 'clicks'),
-    backgroundModule: new BackgroundModule('backgroundModule', 'background')
-}
+    randomAudio: new RandomAudio("randomAudio", "Рандомный звук"),
+    randomFigure: new RandomFigure("randomFigure", "randomFigure"),
+    shapeModule: new ShapeModule("shapeModule", "shape"),
+    clicks: new ClicksModule(),
+    backgroundModule: new BackgroundModule("backgroundModule", "background"),
+    setBackgroundWhite: new SetBackgroundWhiteModule(),
+    guessNumber: new GuessNumberModule(),
+};
 
 function removeModuleResult() {
-    const module = document.querySelector('.module')
+    const module = document.querySelector(".module");
     if (module) {
-        module.remove()
+        module.remove();
     }
 }
 
-document.body.addEventListener('contextmenu', (event) => {
-    event.preventDefault()
-    removeModuleResult()
-    const items = document.querySelectorAll('.menu-item')
+document.body.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+    removeModuleResult();
+    const items = document.querySelectorAll(".menu-item");
 
     if (items.length !== 0) {
         items.forEach((el) => {
-            el.remove()
-        })
-    } 
+            el.remove();
+        });
+    }
 
     Object.assign(menu.el.style, {
         top: `${event.clientY}px`,
-        left: `${event.clientX}px`
-    })
+        left: `${event.clientX}px`,
+    });
 
-    const objKeys = Object.keys(modules)
+    const objKeys = Object.keys(modules);
     if (objKeys.length !== 0) {
-        menu.open()
+        menu.open();
         objKeys.forEach((el) => {
-            menu.add(modules[el])
-        })
+            menu.add(modules[el]);
+        });
     }
-})
+});
 
-menu.el.addEventListener('click', (event) => {
-    removeModuleResult()
-    const {type} = event.target.dataset
-    modules[type].trigger()
-    menu.close()
-})
-
-
-
+menu.el.addEventListener("click", (event) => {
+    removeModuleResult();
+    const { type } = event.target.dataset;
+    modules[type].trigger();
+    menu.close();
+});
