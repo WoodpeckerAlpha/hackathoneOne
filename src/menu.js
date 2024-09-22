0b0078f460cc245b69df3dbb8075f23834161c58
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2024-09-22 13:22:09 +0300

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -1,43 +1,9 @@
 import {Menu} from './core/menu'
-import {BackgroundModule} from './modules/background.module'
-import {ClicksModule} from './modules/clicks.module'
-import { ShapeModule } from './modules/shape.module'
 
 export class ContextMenu extends Menu {
     
     open() {
-        const modules = {
-            shapeModule: new ShapeModule('shapeModule', 'shape'),
-            clicksModule: new ClicksModule('clicksModule', 'clicks'),
-            backgroundModule: new BackgroundModule('backgroundModule', 'background')
-        }
-        
-        document.body.addEventListener('contextmenu', (event) => {
-            event.preventDefault()
-            const items = document.querySelectorAll('.menu-item')
-            if (items.length !== 0) {
-                items.forEach((el) => {
-                    el.remove()
-                })
-            } 
-            Object.assign(this.el.style, {
-                top: `${event.clientY}px`,
-                left: `${event.clientX}px`
-            })
-
-            this.el.classList.add('open')
-
-            this.add(modules['shapeModule'])
-            this.add(modules['clicksModule'])
-            this.add(modules['backgroundModule'])
-
-        })
-
-        this.el.addEventListener('click', (event) => {
-            const {type} = event.target.dataset
-            modules[type].trigger()
-            this.close()
-        })
+        this.el.classList.add('open')
     }
 
     close() {
