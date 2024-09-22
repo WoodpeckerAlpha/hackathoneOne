02ca4acd72c1ac07c49b2ddf0ad2e80f5a6f1da8
sergeyk1150 <128826073+sergeyk1150@users.noreply.github.com>
2024-09-22 16:08:16 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import './styles.css'
 
 import { ContextMenu } from './menu'
-import {BackgroundModule} from './modules/background.module'
-import {ClicksModule} from './modules/clicks.module'
+import { BackgroundModule } from './modules/background.module'
+import { ClicksModule } from './modules/clicks.module'
 import { ShapeModule } from './modules/shape.module'
 import { RandomFigure } from './modules/random.figure.module'
 
